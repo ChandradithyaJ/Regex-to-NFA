33c144789dd0f513db0ca92d3390ad92ac201945
Chandradithya J <110807345+ChandradithyaJ@users.noreply.github.com>
2023-05-27 10:48:11 +0530

    Add files via upload
    Fixed self loop bug

--- Regex to NFA (Python code with Visualizations)/main.py
+++ Regex to NFA (Python code with Visualizations)/main.py
@@ -1,13 +1,13 @@
 import visuals
 
 def main():
-	regex = input("Enter the regular expression: ")
-	visuals.create_digraph(regex)
- 
-	"""
- 	Accepting states = misty rose
-	Start state is labelled 'start'
-	"""
+    regex = input("Enter the regular expression: ")
+    visuals.create_digraph(regex)
+
+    """
+     Accepting states = misty rose
+     {States} - {Accepting States} = sky blue
+    """
 
 if __name__ == '__main__':
-	main()
+    main()
